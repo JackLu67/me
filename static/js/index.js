42b5cf8be91241529d27fb38542d1791a444414a
jack <690137092@qq.com>
2020-03-25 11:10:18 +0800

    first commit

---
+++ static/js/index.js
@@ -0,0 +1,8 @@
+/*
+ * @Author: your name
+ * @Date: 2020-03-20 15:15:24
+ * @LastEditTime: 2020-03-20 16:38:18
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \study\koa2\static\js\index.js
+ */
